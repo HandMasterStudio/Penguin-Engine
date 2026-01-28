b184b5dc5450c866eaa62b18259654e494d26b23
HandMasterStudio <handmasterdev@gmail.com>
2026-01-29 01:04:37 +0800

    first commit 1/29/26

---
+++ build/CMakeFiles/penguin.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/penguin.dir/main.cpp.obj"
+  "CMakeFiles/penguin.dir/main.cpp.obj.d"
+  "libpenguin.dll.a"
+  "penguin.exe"
+  "penguin.exe.manifest"
+  "penguin.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/penguin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout << "hello penguin engine " <<  endl;
+    return 0; 
+}
